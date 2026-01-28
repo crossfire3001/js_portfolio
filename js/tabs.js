c907848d41cd26443f95a14c3692d8b329c0ec98
crossfire3001 <obiwan2035@mail.ru>
2026-01-28 18:38:35 +0300

    working with data attributes using JS

--- js/tabs.js
+++ js/tabs.js
@@ -1,15 +1,27 @@
 const tabButtons = document.querySelectorAll(".design-list__item");
 const tabDesc = document.querySelectorAll(".design__descr");
+const tabImages = document.querySelectorAll(".design-images");
 
 tabButtons.forEach((tabButton, index) => {
   tabButton.addEventListener("click", (e) => {
-    tabDesc.forEach((descr, indexDescr) => {
-      if (index === indexDescr) {
-        descr.classList.remove('hidden') 
+    const dataValue = tabButton.dataset.tabsHandler;
+
+    tabDesc.forEach((descr) => {
+      if (descr.dataset.tabsField === dataValue) {
+        descr.classList.remove("hidden");
       } else {
-        descr.classList.add('hidden');
+        descr.classList.add("hidden");
       }
     });
+
+    tabImages.forEach((image) => {
+      if (image.dataset.tabsField === dataValue) {
+        image.classList.remove("hidden");
+      } else {
+        image.classList.add("hidden");
+      }
+    });
+
     tabButtons.forEach((btn) => {
       if (btn === e.target) {
         btn.classList.add("design-list__item_active");
